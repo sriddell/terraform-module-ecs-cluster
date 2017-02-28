bc394373f2d635bdc287cc60ec1f141cc1790213
Shane Riddell <shane.riddell@ellucian.com>
2017-02-28 10:41:05 -0500

    Make workspace optional

--- templates/container-instance-start.sh
+++ templates/container-instance-start.sh
@@ -52,14 +52,17 @@ EOF
 ./sumo.sh -q -vskipRegistration=true -Vemphemeral=true -Vcollector.name="ecs-${cluster_name}-$instance_id" -Vsources=/opt/SumoCollector/config/sources.json -Vsumo.accessid=suYSVkpXr6QFNd -Vsumo.accesskey=pNpzXqopkBYfAMHcxyJp2jxJhADznMkHUbc7e8Mpnue7WcQSW7s8hgl0bO9e3DqX
 cd /etc/init.d/
 
-yum update
-yum -y install nfs-utils
-DIR_SRC=${workspace_endpoint}
-DIR_TGT=/mnt/efs/workspaces
-mkdir -p $DIR_TGT
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $DIR_SRC:/ $DIR_TGT
-cp -p /etc/fstab /etc/fstab.back-$(date +%F)
-echo -e "$DIR_SRC:/ \t\t $DIR_TGT \t\t nfs \t\t defaults \t\t 0 \t\t 0" | tee -a /etc/fstab
+if [ "${workspace_endpoint}" != "" ];
+then
+    yum update
+    yum -y install nfs-utils
+    DIR_SRC=${workspace_endpoint}
+    DIR_TGT=/mnt/efs/workspaces
+    mkdir -p $DIR_TGT
+    mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $DIR_SRC:/ $DIR_TGT
+    cp -p /etc/fstab /etc/fstab.back-$(date +%F)
+    echo -e "$DIR_SRC:/ \t\t $DIR_TGT \t\t nfs \t\t defaults \t\t 0 \t\t 0" | tee -a /etc/fstab
+fi
 
 
 docker ps #workaround for issue https://github.com/aws/amazon-ecs-agent/issues/389
