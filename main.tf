0d9cc0743ee03cd3261a258c54f970d5f720ab7e
Shane Riddell <shane.riddell@ellucian.com>
2019-07-15 19:18:59 +0000

    Fix terraform 0.12.x upgrade

--- main.tf
+++ main.tf
@@ -191,18 +191,7 @@ resource "aws_launch_configuration" "ecs" {
   iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.id
   associate_public_ip_address = false
   key_name = var.key_name
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
-  security_groups = [concat(
-    var.container_instance_sec_group_ids,
-    [aws_security_group.container_instance.id],
-  )]
+  security_groups = "${var.container_instance_sec_group_ids}"
   user_data = data.template_file.ecs_lc_user_data.rendered
 
   lifecycle {
